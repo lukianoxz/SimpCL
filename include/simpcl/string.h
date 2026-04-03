3cda070d6084c4b5178fa74ec5c4f65a7c279403
lukianoxz <lucasferrer2022@gmail.com>
2026-04-03 18:31:55 -0300

    Added string functions and small refac

--- include/simpcl/string.h
+++ include/simpcl/string.h
@@ -12,12 +12,16 @@ struct scl_string;
 struct scl_string *scl_new_string();
 void               scl_destroy_string(struct scl_string *string);
 
+// information
+int                scl_string_compare(const struct scl_string *string, const struct scl_string *other);
+char              *scl_string_to_cstring(const struct scl_string *string);
+
 // edit functions
 void               scl_string_clear(struct scl_string *string);
 void               scl_string_append_cstring(struct scl_string *string, const char *c_string);
 
 // I/O
-void               scl_string_print(struct scl_string *string);
+void               scl_string_print(const struct scl_string *string);
 void               scl_string_readline(struct scl_string *string);
 
 #endif
--- src/string.c
+++ src/string.c
@@ -28,6 +28,41 @@ void scl_destroy_string(struct scl_string *string) {
     free(string);
 }
 
+// information
+static inline char _scl_string_at(const struct scl_string *string, size_t index) {
+    return *(char *)scl_vector_at(string->data, index);
+}
+
+static inline size_t _scl_string_size(const struct scl_string *string) {
+    return scl_vector_size(string->data);
+}
+
+int scl_string_compare(const struct scl_string *string, const struct scl_string *other) {
+    size_t string_size = _scl_string_size(string);
+    size_t other_size = _scl_string_size(other);
+
+    if (string_size != other_size) return 0;
+
+    for (size_t i = 0; i < string_size; i++) {
+        if (_scl_string_at(string, i) != _scl_string_at(other, i)) return 0;
+    }
+
+    return 1;
+}
+
+char *scl_string_to_cstring(const struct scl_string *string) {
+    size_t string_size = _scl_string_size(string);
+    char *cstring = (char *)malloc(sizeof(char) * (string_size + 1));
+
+    if (!cstring) return NULL;
+
+    void *string_data = scl_vector_at(string->data, 0);
+    cstring[string_size] = '\0';
+    memcpy(cstring, string_data, string_size);
+
+    return cstring;
+}
+
 // edit functions
 void scl_string_clear(struct scl_string *string) {
     scl_vector_clear(string->data);
@@ -35,26 +70,29 @@ void scl_string_clear(struct scl_string *string) {
 
 void scl_string_append_cstring(struct scl_string *string, const char *c_string) {
     size_t cstring_size = strlen(c_string);
+    char c;
 
     for (size_t i = 0; i < cstring_size; i++) {
-        scl_vector_push(string->data, &c_string[i]);
+        c = c_string[i];
+        scl_vector_push(string->data, &c);
     }
 }
 
 // I/O
-void scl_string_print(struct scl_string *string) {
-    char terminator = '\0';
-    scl_vector_push(string->data, &terminator);
+void scl_string_print(const struct scl_string *string) {
+    size_t string_size = _scl_string_size(string);
+    char *string_data = scl_vector_at(string->data, 0);
 
-    char *v_data = scl_vector_at(string->data, 0);
-    fputs(v_data, stdout);
-    scl_vector_pop(string->data, NULL);
+    for (size_t i = 0; i < string_size; i++) {
+        putchar(string_data[i]);
+    }
 }
 
 void scl_string_readline(struct scl_string *string) {
-    char c;
+    int ic;
 
-    while ((c = getchar()) != '\n' && c != EOF) {
+    while ((ic = getchar()) != '\n' && ic != EOF) {
+        char c = (char)ic;
         scl_vector_push(string->data, &c);
     }
 }
